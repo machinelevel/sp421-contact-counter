aac92023aa33170329d6289945887759961d0799
Eric Johnston <ej@machinelevel.com>
2020-07-12 09:34:10 -0400

    [tsu bt] dotch add some bt-connect code

--- tsu/tsu1-code.py
+++ tsu/tsu1-code.py
@@ -4,6 +4,9 @@ import digitalio
 import neopixel
 import adafruit_ble
 
+def addrs_to_hex(addrs):
+    return [''.join('{:02x}:'.format(x) for x in addr)[:-1] for addr in addrs]
+
 led = digitalio.DigitalInOut(board.D13)
 led.direction = digitalio.Direction.OUTPUT
 
@@ -13,6 +16,17 @@ radio = adafruit_ble.BLERadio()
 rssi = -80   # -80 is good, -20 is very close, -120 is very far away
 scan_timeout = 0.25
 unique_contacts=set()
+
+# set up Bluefruit Connect (from ej)
+if 1:
+    from adafruit_ble.advertising.standard import ProvideServicesAdvertisement
+    from adafruit_ble.services.nordic import UARTService
+    from adafruit_bluefruit_connect.packet import Packet
+    uart_server = UARTService()
+    advertisement = ProvideServicesAdvertisement(uart_server)
+    was_connected = False
+    radio.start_advertising(advertisement)
+
 
 pixels = neopixel.NeoPixel(board.NEOPIXEL, 10,
                                brightness=0.2, auto_write=False)
@@ -41,11 +55,13 @@ while True:
     led.value = False
     scan_result = radio.start_scan(timeout=scan_timeout,
                                             minimum_rssi=rssi)
-    contacts = [s.address for s in scan_result]
+    contacts = [s.address.address_bytes for s in scan_result]
     unique_contacts.update(contacts)
     print('contacts to date',len(unique_contacts))
     num_contacts=len(unique_contacts)+debug_test
     print('debug count',num_contacts)
+    debug_text = '{},{} // uniques:{} current:{}\n'.format(len(contacts),len(unique_contacts),len(unique_contacts),addrs_to_hex(contacts))
+    print(debug_text)
 
     #1-50 color 1 (increments of 5)
     #51-250 color 2 (increments of 25)
@@ -67,5 +83,22 @@ while True:
             pixels[i]=(0,0,0)
 
     pixels.show()
+
+    # update Bluefruit Connect
+    if radio.connected:
+        was_connected = True
+        uart_server.write(debug_text.encode())
+        if uart_server.in_waiting:
+            raw_bytes = uart_server.read(uart_server.in_waiting)
+            text = raw_bytes.decode().strip()
+            print("RX:", text)
+            if '143.all' in text:
+                outstr = 'all uniques:\n'
+                for i,addr in enumerate(sorted(list(unique_contacts))):
+                    outstr += ' {}: {}\n'.format(i,addrs_to_hex([addr]))
+                uart_server.write(outstr.encode())
+    elif was_connected:
+        was_connected = False
+        radio.start_advertising(advertisement)
 
     #debug_test+=50
