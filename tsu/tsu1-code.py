e34fbd354e63951ab990152b525b19390a9073de
Eric Johnston <ej@machinelevel.com>
2020-07-12 10:39:11 -0400

    [tsu log] dotch added logging

--- tsu/tsu1-code.py
+++ tsu/tsu1-code.py
@@ -3,9 +3,13 @@ import time
 import digitalio
 import neopixel
 import adafruit_ble
+import os
+import gc
 
+def addr_to_hex(addr):
+    return ''.join('{:02x}:'.format(x) for x in addr)[:-1]
 def addrs_to_hex(addrs):
-    return [''.join('{:02x}:'.format(x) for x in addr)[:-1] for addr in addrs]
+    return [addr_to_hex(addr) for addr in addrs]
 
 led = digitalio.DigitalInOut(board.D13)
 led.direction = digitalio.Direction.OUTPUT
@@ -47,8 +51,51 @@ multipliers=[25]
 multcolors=[(167,8,211),(40,2,200),(2,20,255),(0,255,242),(10,255,38),
             (185,211,12),(211,132,4),(211,66,8),(211,4,7),(255,0,0)]
 
-print('my new project!')
+def btprint(text):
+    print(text)
+    if radio.connected:
+         uart_server.write((text+'\n').encode())
+
+class DoubleLager:
+    def __init__(self):
+        self.log_file_max_size = 100 * 1024
+        self.filenames = ['/data_log0.txt', '/data_log1.txt']
+        self.log_index = 1
+        try:
+            size0 = os.stat(self.filenames[0]).st_size
+            if size0 < self.log_file_max_size:
+                self.log_index = 0
+        except:
+            pass
+
+    def log_str(self, line):
+        '''write a string to the log'''
+        try:
+            with open(self.filenames[self.log_index],'a') as f:
+                f.write(line+'\n')
+                if f.tell() > self.log_file_max_size:
+                    self.log_index = (self.log_index + 1) & 1
+                    with open(self.filenames[self.log_index],'w') as f2:
+                        pass
+        except Exception as ex:
+            btprint('failed to write log: {}'.format(ex))
+
+    def log_dump(self, bt=None):
+        '''Read the logs out to stdout and bt-connect'''
+        for i in range(2):
+            index = (self.log_index + i + 1) & 1
+            try:
+                with open(self.filenames[index],'r') as f:
+                    for line in f:
+                        btprint(line)
+            except Exception as ex:
+                btprint('failed to read log: {}'.format(ex))
+
+lager = DoubleLager()
+btprint('my new project!')
+lager.log_str('startup,{},{}'.format(int(time.monotonic()), len(unique_contacts)))
 while True:
+    gc.collect()
     time.sleep(1.0)
     led.value = True
     time.sleep(0.2)
@@ -56,12 +103,20 @@ while True:
     scan_result = radio.start_scan(timeout=scan_timeout,
                                             minimum_rssi=rssi)
     contacts = [s.address.address_bytes for s in scan_result]
-    unique_contacts.update(contacts)
-    print('contacts to date',len(unique_contacts))
+    for addr in contacts:
+        if not addr in unique_contacts:
+            unique_contacts.add(addr)
+            lager.log_str('add,{},{},{}'.format(int(time.monotonic()),
+                                                len(unique_contacts),
+                                                addr_to_hex(addr)))
+
     num_contacts=len(unique_contacts)+debug_test
-    print('debug count',num_contacts)
-    debug_text = '{},{} // uniques:{} current:{}\n'.format(len(contacts),len(unique_contacts),len(unique_contacts),addrs_to_hex(contacts))
-    print(debug_text)
+    debug_text = '{},{} // uniques:{} current:{} free mem:{}k'.format(len(contacts),
+                                                                      len(unique_contacts),
+                                                                      len(unique_contacts),
+                                                                      addrs_to_hex(contacts),
+                                                                      int(gc.mem_free() // 1024))
+    btprint(debug_text)
 
     #1-50 color 1 (increments of 5)
     #51-250 color 2 (increments of 25)
@@ -91,12 +146,13 @@ while True:
         if uart_server.in_waiting:
             raw_bytes = uart_server.read(uart_server.in_waiting)
             text = raw_bytes.decode().strip()
-            print("RX:", text)
+            btprint("RX:", text)
             if '143.all' in text:
-                outstr = 'all uniques:\n'
+                btprint('all uniques:')
                 for i,addr in enumerate(sorted(list(unique_contacts))):
-                    outstr += ' {}: {}\n'.format(i,addrs_to_hex([addr]))
-                uart_server.write(outstr.encode())
+                    btprint(' {}: {}'.format(i,addrs_to_hex([addr])))
+            elif '143.log' in text:
+                lager.log_dump()
     elif was_connected:
         was_connected = False
         radio.start_advertising(advertisement)
